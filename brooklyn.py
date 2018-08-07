49d928863de100f33524da5b040e5aa4bd3bf3dc
brooklynLife <uglyducklin@naver.com>
2018-08-07 23:36:12 +0900

    Update brooklyn.py

--- brooklyn.py
+++ brooklyn.py
@@ -2,11 +2,18 @@ from gensim.models import FastText, KeyedVectors
 import random
 import re
 import nltk
+import numpy as np
+
+
+model = None
 
 
 def similar(self):
+
     global model
-    model = KeyedVectors.load_word2vec_format('./data/wiki-news/wiki-brooklyn.bin', binary=True, unicode_errors='ignore')
+
+    if model is None:
+        model = KeyedVectors.load_word2vec_format('./data/wiki-news/wiki-brooklyn.bin', binary=True, unicode_errors='ignore')
 #    sim2 = []
 #    sim1 = model.most_similar(self, topn = 25)
 #    sim3 = [sim2.append(word) for word, score in sim1]
@@ -32,10 +39,26 @@ def similar(self):
                 word2 = re.sub('[-=.#/?:$}]', '', word)
                 mostRelated.append(word2)
 
-    return mostRelated
+
+############## serendipity ##################
 
 
-############## MARKOV_CHAIN ##################
+    fast_vocab_pre = model.wv.vocab
+    fast_vocab = list(fast_vocab_pre.keys())
+    noRelated = random.sample(fast_vocab, 10)
+
+    random_related= random.sample(mostRelated, 1)
+    random_unrelated = random.sample(noRelated, 2)
+    serendipity_score = model.most_similar_cosmul(positive=random_unrelated, negative=random_related)
 
 
-# def markov(self):
+    serendipity = []
+    for word, score in serendipity_score:
+        word2 = re.sub('[=.#/?:$}]', '', word)
+        serendipity.append(word2)
+
+ #   return serendipity
+    totalset = mostRelated + serendipity
+    
+    return totalset
+
